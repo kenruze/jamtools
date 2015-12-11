cd532a552d1cacdf8a76de289747858017c80750
elliott davis <kenruze@gmail.com>
2015-12-11 19:44:37 -0400

    animator state behaviour ressetting play time of clip on enter

---
+++ CharacterPuppet/AnimatorStateAlwaysPlayAtZero.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using JamTools;
+
+public class AnimatorStateAlwaysPlayAtZero : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetTime(0);
+    }
+}
